9efd236ee22bc5f79248c365a92d25684a022207
geosh2000 <albert.sanchez@pricetravel.com.mx>
2019-03-01 14:28:02 -0500

    Graficos para formularios

--- application/controllers/Survey.php
+++ application/controllers/Survey.php
@@ -37,7 +37,7 @@ class Survey extends REST_Controller {
             errResponse('Error en DB', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', $this->db->error());
         }
 
-      if( !$opt = $this->db->select('a.*')
+        if( !$opt = $this->db->select('a.*')
             ->from('form_Opts a')
             ->join('form_Fields b', 'a.fieldId=b.id', 'left')
             ->where('masterId', $id)
@@ -46,7 +46,15 @@ class Survey extends REST_Controller {
             errResponse('Error en DB', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', $this->db->error());
         }
 
-        okResponse( 'Información de Survey Obtenida', 'data', array( 'master' => $master->row_array(), 'fields' => $fields->result_array(), 'opts' => $opt->result_array()), $this );
+        if( !$graph = $this->db->select('*')
+            ->from('form_sankeyParams a')
+            ->where('masterId', $id)
+            ->limit(1)
+            ->get() ){
+            errResponse('Error en DB', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', $this->db->error());
+        }
+
+        okResponse( 'Información de Survey Obtenida', 'data', array( 'master' => $master->row_array(), 'fields' => $fields->result_array(), 'opts' => $opt->result_array(), 'graph'=>$graph->row_array()), $this );
 
     });
   }
@@ -165,6 +173,88 @@ class Survey extends REST_Controller {
                 }
             }
                 okResponse( 'Historial Obtenido', 'data', $result, $this );
+        }else{
+                errResponse('Error al guardar formulario', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', $this->db->error());
+        }
+
+    });
+  }
+
+  public function sankey_get(){
+
+    $result = validateToken( $_GET['token'], $_GET['usn'], $func = function(){
+
+        $master = $this->uri->segment(3);
+        $inicio = $this->uri->segment(4);
+        $fin = $this->uri->segment(5);
+
+        $err = false;
+
+        if( !$pQ = $this->db->query("SELECT * FROM form_sankeyParams WHERE masterId=$master ORDER BY step") ){
+            $err = true;
+        }
+
+        if( $pQ->num_rows() <= 0 ){
+            $err = true;
+        }
+        
+        if( $err ){
+            errResponse('Error al cargar parámetros del formulario', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', '');
+        }
+
+        $this->db->query("SET @inicio = '$inicio 00:00:00'");
+        $this->db->query("SET @fin = '$fin 23:59:59'");
+        $this->db->query("SET @master = $master");
+
+        $this->db->query("DROP TEMPORARY TABLE IF EXISTS sankey");
+
+        $query = "CREATE TEMPORARY TABLE sankey ";
+
+        foreach( $pQ->result_array() as $index => $info ){
+            if( $info['step'] != 1 ){
+                $query .= " UNION ";
+            }
+
+            if( isset($info['isNull']) ){
+                $isNullVal = $info['isNull']." ";
+            }else{
+                $isNullVal = "NULL";
+            }
+
+            $query .= "(SELECT 
+                    COALESCE(CONCAT('".$info['prefixA']."',".$info['nameVal']."),".$isNullVal.") AS s_A, CONCAT('".$info['prefixB']."',".$info['nameVal_B'].") AS s_B, COUNT(*) AS q
+                FROM
+                    form_dataBase a ";
+            
+            if($info['join'] == '1'){
+                $query .= " LEFT JOIN
+                ".$info['joinTable']." ON ".$info['rightVal']." = ".$info['leftVal'];
+            }
+
+            if($info['join_B'] == '1'){
+                $query .= " LEFT JOIN
+                ".$info['joinTable_B']." ON ".$info['rightVal_B']." = ".$info['leftVal_B'];
+            }
+                        
+            $query .= " WHERE
+                    master = @master AND dtCreated BETWEEN @inicio AND @fin
+                GROUP BY s_A , s_B
+                HAVING s_A IS NOT NULL AND s_B IS NOT NULL)";
+        }
+
+        $this->db->query($query);
+
+        $qF = "SELECT * FROM sankey";
+        
+        if( $r = $this->db->query($qF) ){
+            // okResponse( 'Historial Obtenido', 'data', $r->result_array(), $this );
+            
+            $result = array();
+            foreach( $r->result_array() as $index => $info ){
+                array_push($result, array($info['s_A'],$info['s_B'],intVal($info['q'])));
+            }
+            okResponse( 'Historial Obtenido', 'data', $result, $this );
+
         }else{
                 errResponse('Error al guardar formulario', REST_Controller::HTTP_BAD_REQUEST, $this, 'error', $this->db->error());
         }
